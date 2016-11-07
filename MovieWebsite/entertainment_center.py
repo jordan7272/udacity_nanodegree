2b1a70fe296be086ba3933449199c15d79019c09
jordan7272 <jordan7272@yahoo.com>
2016-11-07 12:33:46 -0800

    Update based on Udacity review 11/7

--- MovieWebsite/entertainment_center.py
+++ MovieWebsite/entertainment_center.py
@@ -1,17 +1,22 @@
-import media            #media file contains class definition of Movie
-import fresh_tomatoes   #fresh_tomatoes.py file contains open_movies_page function used to create html file
+#media file contains class definition of Movie
+import media
+#fresh_tomatoes.py file contains open_movies_page function used to create html file
+import fresh_tomatoes
 
+#create dodgeball object
 dodgeball = media.Movie("Dodgeball",
                         "https://az616578.vo.msecnd.net/files/responsive/cover/main/desktop/2016/08/21/636074048726998131436357571_dodgeball.jpg",
                         "http://www.pastposters.com/cw3/assets/product_full/R/dodgeball-cinema-quad-movie-poster-(teaser-2).jpg",
-                        "https://www.youtube.com/watch?v=W-XbDZUnUmw")          #create dodgeball object
+                        "https://www.youtube.com/watch?v=W-XbDZUnUmw")
 
+#create napoleon_dynamite object
 napoleon_dynamite = media.Movie("Napoleon Dynamite",
                         "https://az616578.vo.msecnd.net/files/responsive/cover/main/desktop/2016/03/11/6359326647185971841926479363_Napoleon-Dynamite.jpg",
                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNjYwNTA3MDIyMl5BMl5BanBnXkFtZTYwMjIxNjA3._V1_UX182_CR0,0,182,268_AL_.jpg",
-                        "https://www.youtube.com/watch?v=qH-FBPRf7NQ")          #create napoleon_dynamite object
+                        "https://www.youtube.com/watch?v=qH-FBPRf7NQ")
 
+#create array with name of Movie objects
+movies = [dodgeball, napoleon_dynamite]
 
-movies = [dodgeball, napoleon_dynamite]         #create array with name of Movie objects
-
-fresh_tomatoes.open_movies_page(movies)         #pass array of Movie objects to open_movies_page function 
+#pass array of Movie objects to open_movies_page function
+fresh_tomatoes.open_movies_page(movies)         
--- MovieWebsite/media.py
+++ MovieWebsite/media.py
@@ -1,8 +1,9 @@
 import webbrowser
 
 class Movie():
-"""Create a Movie object"""
+    """Class defintion of Movie object"""
     def __init__(self, title, art, poster_url, youtube_url):
+        """Constructor method to create a Movie instance"""
         self.title = title
         self.art = art
         self.poster_image_url = poster_url
